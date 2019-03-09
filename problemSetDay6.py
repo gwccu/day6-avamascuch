929e34ef498976c2a3ef5fdc757fe0bf729eb80e
avamascuch <avamascuch@gmail.com>
2019-03-09 17:32:26 +0000

    yayy

--- problemSetDay6.py
+++ problemSetDay6.py
@@ -1,1 +1,46 @@
 # File name: problemSetDay6.py
+for i in range(5):
+    print(i)
+
+for i in range (5,8):
+    print(i)
+
+for i in range (5,18):
+    print(i)
+
+for i in range (0,31):
+    print(i)
+
+count = 0
+while count <= 4:
+    print(count)
+    count += 1
+
+count = 5
+while count <= 7:
+    print(count)
+    count += 1
+
+count = 5
+while count <= 17:
+    print(count)
+    count <= 3
+
+count = 0
+while count <= 30:
+    print(count)
+    count <= 5
+
+for i in range (1):
+    print("   * ")
+for i in range (1):
+    print("  *** ")
+for i in range (1):
+    print(" ***** ")
+for i in range (1):
+    print("*******")
+
+for i in range (1500,1701,5):
+    if i%7== 0:
+        print(i)
+
